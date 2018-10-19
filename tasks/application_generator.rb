bfc953ac6954fa0ef2aeec91188c846e97c41f3f
David Rodríguez <deivid.rodriguez@riseup.net>
2018-10-19 17:36:21 -0300

    Move parallel DB setup to the generator as well

--- tasks/application_generator.rb
+++ tasks/application_generator.rb
@@ -35,11 +35,16 @@ module ActiveAdmin
 
         Bundler.with_original_env { Kernel.exec(env, command) }
 
-        Rake::Task['parallel:after_setup_hook'].invoke if parallel
+        rails_app_rake "parallel:load_schema" if parallel
       end
     end
 
     private
+
+    def rails_app_rake(task)
+      require 'rails/version'
+      system "cd spec/rails/rails-#{Rails::VERSION::STRING}; rake #{task}"
+    end
 
     def app_dir
       @app_dir ||= begin
